122a68e2887bfdd610ee06dcdd17258b3959316a
Gui-dot577 <adeilgosantos@gmail.com>
2024-09-22 10:32:20 -0300

    adicionei arquivo via upload

---
+++ Projetogithub/src/br/com/joseassis/git/Hellogit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+ package br.com.joseassis.git;
+
+/**
+ * @author joé de Assis
+ * Exemplo de integraçao do Eclipse com o GitHub
+*/
+ public class Hellogit {
+
+     public static void main(String[] args) {
+		 System.out.println("Hello Git");
+		
+	}
+
+}
